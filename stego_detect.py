e15c7294debcd4a55a8250009a1287e62157428d
Rndmun <91401175+Rndmun@users.noreply.github.com>
2026-04-25 11:52:12 -0700

    realizing its printing way too much, terminal froze

--- stego_detect.py
+++ stego_detect.py
@@ -32,7 +32,7 @@ def analyze_structure(filepath):
                     else:
                         diff = abs(p[0] - right_pixel[0])
                     if diff > 100:
-                        print('anomaly', x, y, diff)
+                        # print('anomaly', x, y, diff) # WAY TOO MUCH OUTPUT
     except Exception as e:
         print("pillow error:", e)
 
